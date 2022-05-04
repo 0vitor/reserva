c6c8e51f6666233af84e4c581e378bf7acf44604
vitor <w.wutaes@gmail.com>
2022-05-03 21:06:08 -0300

    wip

--- main.py
+++ main.py
@@ -1,3 +1,2 @@
 from reserva import Reserva 
 
-print(Reserva('sala', 'filme', 'dia', 'sessao'))    
--- sala.py
+++ sala.py
@@ -1,3 +1,11 @@
 class Sala:
     def __init__(self, numero, filme, sessoes, filas, cadeiras):
-        pass
+        self.__numero = numero
+        self.__filme = filme
+        self.__sessoes = sessoes
+        self.__arranjoCadeiras = self.__gerarCadeira(filas, cadeiras)
+        self.__filas = filas
+        self.__cadeiras = cadeiras
+    
+    def __gerarCadeiras(self, filas, cadeiras):
+      return [[{ocupado: False}] * filas for _ in range(cadeiras)]
